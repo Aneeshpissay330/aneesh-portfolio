a1f0e2717b6a64e857705b94aac29fb538663a58
Aneeshpissay <126843748+Aneeshpissay330@users.noreply.github.com>
2024-07-12 12:45:55 +0530

    changed project link

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -28,7 +28,7 @@ const Projects = () => {
                             initial={{ x: 100, opacity: 0 }}
                             transition={{ duration: 1 }}
                             className="w-full max-w-xl lg:w-3/4">
-                            <h6 onClick={() => openInNewTab("https://todo-list-divf.onrender.com/")} className="mb-2 font-semibold cursor-pointer underline">{project.title}</h6>
+                            <h6 onClick={() => openInNewTab(project.link)} className="mb-2 font-semibold cursor-pointer underline">{project.title}</h6>
                             <p className="mb-4 text-neutral-400">{project.description}</p>
                             {project.technologies.map((tech: string, index: number) => (
                                 <span key={index} className="mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-[#FFA500]">{tech}</span>
